d11bcbd151c0c88efb811ca00438cdf74c3a48c6
Andy DeVane <w.andrew.dev@gmail.com>
2017-06-21 20:01:21 -0400

    hotfix

--- public/javascript/frontend.js
+++ public/javascript/frontend.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var checkbox = $('#autoplay-input');
     checkbox.trigger('click');
     if (booleanValue() == true) {
-      if (player.getPlayerState == 0) {
+      if (player.getPlayerState() == 0) {
         setTimeout(tryNext(videoList[integer += 1].url, videoList[integer].title), 0);
       };
     };
